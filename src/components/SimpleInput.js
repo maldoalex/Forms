f9236922f3f2a65f45fb928ead954e9b38712582
maldoalex <maldoalex@gmail.com>
2022-02-16 12:14:52 -0600

    Implemented useState to form inputs

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,39 @@
+import {useState} from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === '') {
+    setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+    console.log(enteredName);
+    setEnteredName('');
+  };
+
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input 
+          type='text'
+          id='name' 
+          onChange={nameInputChangeHandler} 
+          value={enteredName}
+        />
+        {!enteredNameIsValid && <p className='error-text'>Name must be valid.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
